b9b46972aaecba465bf0ad783525df67ce40a356
eloiinacio-maker <eloi.inacio@Aluno.senai.br>
2026-09-24 21:33:02 -0300

    aula 24.09.26.32

--- Exercicios/src/Prog1.java
+++ Exercicios/src/Prog1.java
@@ -5,16 +5,25 @@ public class Prog1 {
         Scanner leitura = new Scanner(System.in);
         // pede pra digitar uma palavra varias vezes
         // so para quando a pessoa responder "banana"
+        // Desafio 1 -  imprima o numero da tentativa antes de digitar
+        // Desafio 2 - Se errar 3 vezes , encerrar e dizer que a palavra secreta era "banana"
         String texto;
         int voltas = 0;
         do {
 
-            System.out.println("digite qualquer palavra: ");
+            System.out.println("digite qualquer palavra (1 chance): ");
             texto = leitura.next();
             voltas++;// Soma mais 1 em voltas
-
-        } while(!texto.equalsIgnoreCase("banana"));
+if (voltas<3){
+    voltas--;
+    System.out.print("digite uma palavra ("+voltas+"chance: ");
+    break;
+}
+        } while( (voltas<=3)&&(!texto.equalsIgnoreCase("banana")) );
         System.out.print("Voce tentou "+voltas+" vezes\n");
+        if (texto.equalsIgnoreCase("banana")){
+            System.out.println(" Parabens vc acerto ! :  ");
+        }
     }
 
 }
